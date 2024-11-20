d200507c00e55ca6765d10d11418ebb13f2f638c
Sirius <anyf@163.com>
2024-11-20 21:46:17 +0800

    简记补充

---
+++ 使用简记.py
@@ -0,0 +1,19 @@
+#直接导入GitHub里的文件夹
+#git clone
+
+#管理一个本地文件夹
+#git init 
+
+#提交目前的代码,"."表示文件夹里所有文件；"xxx"对本次提交进行备注；
+#git add .
+#git commit -m "xxx"
+
+#提交一个代码
+#git add 文件名
+#git commit -m "xxx"
+
+#查看提交历史记录
+#git log
+
+#恢复代码
+#git checkout HEAD 文件全名
